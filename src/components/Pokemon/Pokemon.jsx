3d1b3dec629ff735928c70c28500301f90d8cf58
Rubcs <Rubcs@gmail.com>
2023-06-06 15:54:02 +0200

    add ternario for displaying an image in case there's no pokemon

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -3,19 +3,16 @@ import "./Pokemon.css";
 
 const Pokemon = () => {
   const [pokemon, setPokemon] = useState();
-  const [pokemonName, setPokemonName] = useState("pikachu");
   const [searchInput, setSearchInput] = useState("");
 
   const getPokemon = async () => {
     const result = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+      `https://pokeapi.co/api/v2/pokemon/${searchInput}`
     );
     const res = await result.json();
     setPokemon(res);
-    setSearchInput(""); // Clear the input field value
+    setSearchInput("");
   };
-
-  console.log(pokemonName);
 
   return (
     <>
@@ -25,22 +22,28 @@ const Pokemon = () => {
             className="searchInput"
             type="text"
             placeholder="Search your pokemon"
-            value={searchInput} // Bind the input value to the searchInput state
-            onChange={(ev) => setSearchInput(ev.target.value)} // Update the searchInput state on input change
+            value={searchInput}
+            onChange={(ev) => setSearchInput(ev.target.value)}
           />
           <button type="submit" className="searchButton" onClick={getPokemon}>
             Search
           </button>
         </div>
-        {pokemon && (
+        {pokemon ? (
           <div className="pokemonWrapper" key={pokemon.name}>
             <img
               className="pokemonImage"
               src={pokemon.sprites.other.dream_world.front_default}
               alt={pokemon.name}
             />
-            <p>{pokemon.name.toUpperCase()}</p>
+            <p className="pokeName">{pokemon.name.toUpperCase()}</p>
           </div>
+        ) : (
+          <img
+            className="pokemonImage"
+            src="../../../src/assets/main_Img.jpeg"
+            alt="Pokemon"
+          />
         )}
       </div>
     </>
